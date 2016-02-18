f9417fad0a739d74a6b1fd79fa1a9b50695e85b6
pprintz <p-printz@hotmail.com>
2016-02-18 13:05:32 +0100

    dijkstra

---
+++ dijkstra/ConsoleApplication2/Graph.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2 {
+    class Graph {
+        public Vertex initial;
+        public Vertex current;
+        public Vertex dest;
+        public string path;
+        List<Vertex> vertices = new List<Vertex>();
+        List<Vertex> unvisitedVertices = new List<Vertex>();
+        public Graph(Vertex source, Vertex dest, List<Vertex> vertices) {
+            this.initial = source;
+            this.dest = dest;
+            this.vertices = vertices;
+        }
+        public void dijkstra() {
+            foreach (Vertex vertex in vertices) {
+                unvisitedVertices.Add(vertex);
+            }
+            initial.lenFromSource = 0;
+            while(unvisitedVertices.Count != 0) {
+                Vertex current = unvisitedVertices[0];
+                unvisitedVertices.Remove(current);
+                if (current == dest) {
+                    break;
+                }
+                checkEgdes(current);
+            }
+        }
+        public void checkEgdes(Vertex vertex) {
+            foreach (Egde egde in vertex.egdeList) {
+                 if (egde.length + vertex.lenFromSource < egde.destVertex.lenFromSource) {
+                    egde.destVertex.lenFromSource = egde.length + vertex.lenFromSource;
+                    egde.destVertex.parent = egde.sourceVertex;
+                    }
+                } 
+        } 
+        public void printPathAndLength() {
+            Vertex parenthee = dest;
+            List<string> path = new List<string>();
+            Console.WriteLine();
+            do {
+                path.Add(parenthee.name);
+                parenthee = parenthee.parent;
+            } while (parenthee.parent != null);
+            path.Reverse();
+            Console.Write($"{initial.name} --> ");
+            foreach (string item in path) {
+                if (item != path[path.Count-1]) {
+                    Console.Write(item + " --> ");
+                }
+                else {
+                    Console.Write(item);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Length from {initial.name} to {dest.name} is:\n{dest.lenFromSource}");
+        }
+    }
+}
---
+++ dijkstra/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("List of vertices");
+            List<Vertex> vertices = new List<Vertex>();
+            Vertex a = new Vertex(0, 0, "a");
+            Vertex b = new Vertex(3, 0, "b");
+            Vertex c = new Vertex(1, -1, "c");
+            Vertex d = new Vertex(2, -6, "d");
+            Vertex e = new Vertex(2, -8, "e");
+
+            vertices.Add(a);
+            vertices.Add(b);
+            vertices.Add(c);
+            vertices.Add(d);
+            vertices.Add(e);
+            
+            c.addEgde(c, a);
+            c.addEgde(c, b);
+            c.addEgde(c, d);
+            a.addEgde(a, e);
+            b.addEgde(b, e);
+            d.addEgde(d, e);
+
+
+            Graph graf = new Graph(c, e, vertices);
+            Graph graft= new Graph(a, e, vertices);
+
+            foreach (Vertex vertex in vertices) {
+                Egde.printEgde(vertex);
+            }
+            graft.dijkstra();
+            graft.printPathAndLength();
+            Console.ReadKey();
+        }
+    }
+}

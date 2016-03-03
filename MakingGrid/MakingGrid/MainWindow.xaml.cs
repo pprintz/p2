3d39fefc457325e172cb395f6364e2fc6d511b1f
KaiserLorentzen <benjamin.lorentzen@hotmail.com>
2016-03-03 14:23:52 +0100

    Dynamisk nu

--- MakingGrid/MakingGrid/MainWindow.xaml.cs
+++ MakingGrid/MakingGrid/MainWindow.xaml.cs
@@ -25,6 +25,10 @@ namespace MakingGrid
         {
             InitializeComponent();
         }
+
+        Button btn;
+        List<Button> btnList = new List<Button>();
+
         public class RandomColor
         {
             Type colorType = typeof(System.Drawing.Color);
@@ -41,7 +45,8 @@ namespace MakingGrid
                 return ColorName;
             }
         }
-        private void Windom_Loaded(object sender, RoutedEventArgs e)
+        
+        public void Windom_Loaded(object sender, RoutedEventArgs e)
         {
 
             for (int C = 0; C < 50; C++)
@@ -58,10 +63,10 @@ namespace MakingGrid
             }
 
 
-            Button btn;
+            //Button btn;
             int i = 0;
             RandomColor randomColor = new RandomColor();
-
+            
             for (int row = 0; row < MyGrid.RowDefinitions.Count; row++)
             {
                 for (int column = 0; column <  MyGrid.ColumnDefinitions.Count; column++)
@@ -71,25 +76,56 @@ namespace MakingGrid
 
                     btn.Width = Width / MyGrid.ColumnDefinitions.Count;
                     btn.Height = Height / MyGrid.RowDefinitions.Count;
-                        ;
+                        
 
 
-                    //btn.Content = i.ToString();
+                    //rect. = i.ToString();
 
                     BrushConverter converter = new BrushConverter();
                     Brush brush = (Brush)converter.ConvertFromString(randomColor.NextColor().Name);
+                    //rect.Fill = new SolidColorBrush(Colors.Red);
                     btn.Background = brush;
 
                     //btn.FontSize = 0;
                     btn.Margin = new Thickness(0);
-                    btn.Padding = new Thickness(0);
+                    //btn.Padding = new Thickness(0);
                     btn.VerticalAlignment = VerticalAlignment.Center;
                     btn.SetValue(Grid.ColumnProperty, column);
                     btn.SetValue(Grid.RowProperty, row);
                     MyGrid.Children.Add(btn);
+                    btnList.Add(btn);
                 }
             }
         }
-    }
+
+        //public void isWindowSizeChanged(Button btn)
+        //{
+        //    Button btnnew = new Button();
+        //    //int btnwidth;
+        //    //btnwidth = (int)GridLength.Auto;
+        //    btnnew.Width = GridLength.Auto.Value;
+        //    btnnew.Height = GridLength.Auto.Value;  
+        //}
+
+        private void MyGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //int btnwidth;
+            //btnwidth = (int)GridLength.Auto;
+            //btn.Width = GridLength.Auto.Value;
+            //btn.Height = GridLength.Auto.Value;
+            //ResizeMode btn = ResizeMode.CanResizeWithGrip;
+            foreach (Button btn in btnList)
+            {
+                new GridLength(1, GridUnitType.Star);
+                //btn.ActualWidth = GridLength.Auto.Value;
+                //ActualHeight = GridLength.Auto.Value;
+                btn.Width = 1000;
+                btn.Height = 1000;
+                //btn.Height = GridLength.Auto.Value;
+                //btn.Width = GridLength.Auto.Value;
+            }
+            
+        }
+    }   
    
 }
